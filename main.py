decbde89e67f20539109cab854f9c6bf1fd25389
Praveennaik6 <praveennaik0814@gmail.com>
2025-12-05 23:45:08 +0530

    adding image path

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import base64
 import requests
 import io
-import PIL import Image
+from PIL import Image
 from dotenv import load_dotenv
 import os
 import logging
@@ -11,15 +11,18 @@ logger = logging.getLogger(__name__)
 
 load_dotenv()
 
-GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completions -s \"
+GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completions"
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 if not GROQ_API_KEY:
     raise ValueError("GROQ_API_KEY is not set in the .env file")
 
+    def process_image(image_path, query):
+        pass
+
 if __name__=="__main__":
-    image_path = ""
+    image_path = "image_1.jpg"
     query="what are the encoders in this picture?"
     result=process_image(image_path, query)
     print (result)
