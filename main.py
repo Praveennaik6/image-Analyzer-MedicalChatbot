72d1297ec3e1c12e7ec2a5f6e045b007d338b739
Praveennaik6 <praveennaik0814@gmail.com>
2025-12-05 23:36:28 +0530

    adding function

--- main.py
+++ main.py
@@ -9,6 +9,17 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+load_dotenv()
+
 GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completions -s \"
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+if not GROQ_API_KEY:
+    raise ValueError("GROQ_API_KEY is not set in the .env file")
+
+if __name__=="__main__":
+    image_path = "path/to/your/image.jpg"
+    query="what are the encoders in this picture?"
+    result=process_image(image_path, query)
+    print (result)
